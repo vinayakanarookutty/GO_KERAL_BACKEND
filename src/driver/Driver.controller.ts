96bec126d8ac00e72e1d9abb654b91bea3a413b4
Aadhil Abdulla <aadhil242@gmail.com>
2025-02-21 14:31:21 +0530

    DriverList GET added

--- src/driver/Driver.controller.ts
+++ src/driver/Driver.controller.ts
@@ -53,4 +53,18 @@ export class DriverController {
         }
     }
 
+
+    @Get("driverList")
+    async getAll() {
+        
+        try{
+            const users = this.driverService.findAll()
+            return { users }
+        }catch(error){
+            console.log("Login error" , error)
+            throw new HttpException(error.message , HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+
 }
