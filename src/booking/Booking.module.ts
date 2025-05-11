f75c3d51f48b51344afdaffc8bbcdd23916b6f84
Albin Aji <albinpariyarathu@gmail.com>
2025-05-11 19:33:10 +0530

    path issue in booking controller and service resolved

--- src/booking/Booking.module.ts
+++ src/booking/Booking.module.ts
@@ -1,27 +1,21 @@
 /* eslint-disable prettier/prettier */
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { BookingController } from './booking.controller';
-import { BookingService } from './booking.service';
+import { BookingController } from './Booking.controller';
+import { BookingService } from './Booking.service';
 import { Booking, BookingSchema } from '../schemas/Booking.schema';
 import { AuthMiddleware } from 'src/middlleware/auth.middlllleware';
 
-
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Booking.name, schema: BookingSchema }])
+    MongooseModule.forFeature([{ name: Booking.name, schema: BookingSchema }]),
   ],
   controllers: [BookingController],
   providers: [BookingService],
   exports: [BookingService],
 })
 export class BookingModule {
-     configure(consumer: MiddlewareConsumer) {
-        consumer
-          .apply(AuthMiddleware)
-          .forRoutes(
-            '/bookings',
-            '/bookingDetails'
-          ); 
-      }
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('/bookings', '/bookingDetails');
+  }
 }
